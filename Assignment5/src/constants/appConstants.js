edf7cf21204a06ff5ee3e130e03ea4e5aa4ed758
A-Pattanayak <aniketedsh@gmail.com>
2026-06-09 23:07:29 +0530

    Add assignment 14

--- Assignment5/src/constants/appConstants.js
+++
@@ -1,8 +0,0 @@
-export const APP_TITLE = "Task Management";
-export const APP_SUBTITLE = "Manage your daily goals.";
-export const TODO_PLACEHOLDER = "What needs to be done today?";
-export const EMPTY_TODO_MESSAGE = "No tasks yet. Add a task to get started!";
-export const TODO_STATUS = {
-  completed: "Completed",
-  pending: "In Progress",
-};
